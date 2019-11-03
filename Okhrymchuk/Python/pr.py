1b6a5af617690d885241f1e063e61c5e83ce83fa
Dennis <32113099+ElKoriano@users.noreply.github.com>
2019-11-03 23:38:18 +0200

    Update pr.py

--- Okhrymchuk/Python/pr.py
+++ Okhrymchuk/Python/pr.py
@@ -3,16 +3,3 @@ from interface import *
 
 interface=Interface()
 interface.start_work()
-#cs=Countries()
-#cs.add_country("country1", Coord2D(0,0), Coord2D(1,1))
-#cs.add_country("country2", Coord2D(1,1), Coord2D(2,2))
-#cs.add_country("country2", Coord2D(2,2), Coord2D(3,2))
-#cs.add_country("country3", Coord2D(4,2), Coord2D(5,2))
-#print(cs.check_rules())
-#print(cs.check_all_towns_ended())
-#for c in cs.country_dict:
-#    print(c)
-#print(len(cs.town_list))
-#for t in cs.town_list:
-#    print(t.money_dictionary)
-#print(cs.day_step())
